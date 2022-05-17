0537c3e185b5ad81a069f6aa7cc8efd58ca0da58
freeman-kuch <freemanoparaocha@gmail.com>
2022-05-17 13:34:42 +0100

    practice project for sending 1week scheduled E-mail of randomly selected chores

--- Random Chore Assignment.py
+++ Random Chore Assignment.py
@@ -2,14 +2,39 @@
 # Random Chore Assignment Emailer
 # this code randomly selects a chore from a list and sends the chore as a E-mail
 
-import random, ezgmail, os
+import random, ezgmail, os, time, datetime
 
-os.chdir('C://Users//opara//PycharmProjects')
-listOfEmails = ['freemanoparaocha@gmail.com', 'newmanchidike@yahoo.com', 'prochess91@gmail.com', 'mglory360@ymail.com']
-chores = ['reformatting files', 'front-end JavaScript', 'cleaning', 'cooking', 'Driving']
-randomChore = random.choice(chores)
-
+os.chdir('C://Users//opara//PycharmProjects')  # CWD for my credentials.json
+listOfEmails = ['freemanoparaocha@gmail.com', 'newmanchidike@yahoo.com', 'prochess91@gmail.com',
+                'mglory360@ymail.com', 'patrickufoeze2@gail.com']
 print('welcome to Random chore assignment')
-print('Generating chores ...')
+print('Generating chores\n')
 
-ezgmail.init()
+
+def emailChore():
+    for idx, Emails in enumerate(listOfEmails):
+        chores = ['reformatting files', 'front-end JavaScript', 'Backend development', 'cooking', 'Driving',
+                  'Robotics process automation']
+        randomChore = random.choice(chores)
+        ezgmail.send(listOfEmails[idx], 'Your Chore has been Generated',
+                     f'{time.ctime()}\n\nDear Reader, \n\nYour chore has been selected at Random.\n\nyour chore is "{randomChore}".\n\nPlease Go right ahead and begin your Chore! \n\nkind Regards,\nFreeman :).')
+        chores.remove(randomChore)
+        print(f'sending chore to {Emails} ...')
+        time.sleep(1)
+        print('sent')
+        time.sleep(0.5)
+
+
+def weeklyLoop():
+    timeFrame = datetime.timedelta(weeks=1, days=0, hours=0, minutes=0, seconds=0)  # this helps define the 1week timer
+    secondstimeFrame = timeFrame.total_seconds()  # converts to seconds
+    while secondstimeFrame > 0:
+        time.sleep(1)
+        secondstimeFrame = secondstimeFrame - 1
+
+
+while True:
+    ezgmail.init()
+    emailChore()
+    print(f'Chores Distribution Done on {str(datetime.datetime.now())}')
+    weeklyLoop()
