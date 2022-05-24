0b5c222a10eadf1cb549c17283fb3a0a333d3d22
freeman-kuch <freemanoparaocha@gmail.com>
2022-05-24 21:00:15 +0100

    Regex Practice

---
+++ RegularExpressions.py
@@ -0,0 +1,12 @@
+#! we are writing the date in DD\MM\YYYY format
+import re
+
+dateRegex = re.compile(r'''(
+                            ([0-3]?\d)
+                            (/)
+                            ([0-1]\d)
+                            (/)
+                            ([0-2]\d{3})$
+                            )''', re.VERBOSE)
+mo = dateRegex.search('today\'s date is 5/11/1998')  # this is where the text to be scanned will be input as string
+print(mo.group())
